420dc2c9f67454bbe62a13566d5ceb1c88aeb86f
Anne-Laure <110533977+annlaur@users.noreply.github.com>
2022-10-21 16:06:06 +0200

    connexion

--- connexionTemp.php
+++ connexionTemp.php
@@ -1,4 +1,5 @@
 <?php
+require("test.php");
     $pdo = new PDO('mysql:host=localhost:3307;dbname=lbc', 'root', '');
 ?>
 <!DOCTYPE html>
@@ -12,21 +13,23 @@
 </head>
 <body>
   
-
-    <form action="" method="post">
+<div class="container border border-secondary border-opacity-10 text-center col-4 mt-5 p-3 rounded-5">
+    <form  action="" method="post">
         <div class="connexion">
             <h2>Se connecter</h2>
-            <div class="form-group">
-            <input type="email" name="mail" placeholder="mail"required class="form-control">
+            <div class="form-group mb-3">
+            <input type="email" name="mail" placeholder="mail"required class="form-control rounded-4">
             </div>
-            <div class="form-group">
-            <input type="password" name="mdp"placeholder="mot de passe" required class="form-control">
+            <div class="form-group mb-3">
+            <input type="password" name="mdp"placeholder="mot de passe" required class="form-control rounded-4">
             </div>
-            <input type="submit" name="bout"value="se connecter" class="btn btn-primary">
+            <input type="submit" name="bout"value="se connecter" class="btn btn-primary mb-3 rounded-3">
+            <br>
             <a href="mdp.php">Mot de passe oublié ?</a>
         </div>
         
     </form>
+    </div>
     <?php
         if(isset($_POST["bout"])){
             //verifier si tout est bon
