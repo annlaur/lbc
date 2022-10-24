f9ef6347ef95895ffb34d85a99a6bdc37ec5181c
eloiisehenry <101328483+eloiisehenry@users.noreply.github.com>
2022-10-24 20:31:37 +0200

    detail_annonce
    update

---
+++ detail_annonce.php
@@ -0,0 +1,44 @@
+<?php
+require("session.php");
+require("fonction.php");
+require_once("header.php");
+
+if(isset($_GET['ida']) && !empty($_GET['ida']))
+{
+    $ida = $_GET['ida'];
+    $annonce = getUneAnnonce($pdo, $ida);
+    // $annonce= $statement->fetch();
+    // $request = $statement;
+    // var_dump($annonce);
+}else{
+    header('location : prcp_annonce.php');
+}
+
+$region = getRegion($pdo, $annonce['idu']);
+$image = getUneImage($pdo, $annonce['ida'],$annonce['img']);
+$user = getUser($pdo, $annonce['idu']);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=S, initial-scale=1.0">
+    <title><?= $annonce["titre"]?></title>
+</head>
+<body>
+    <h1><?=$annonce["titre"]?></h1><br>
+    
+    <img src="<?= $image?>"><br>
+    <h4><?= $user["ville"]?></h4><h4><?= $region ?></h4><br>
+    <h4><?= $user["cp"]?></h4><br>
+    <p><?= $annonce["lib_a"]?></p>
+
+    <a href="messagerie.php">CONTACTER le vendeur</a>
+
+
+</body>
+</html>
