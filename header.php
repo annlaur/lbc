675202e5f9d4d9937df45ac55d0049791181a241
eloiisehenry <101328483+eloiisehenry@users.noreply.github.com>
2022-10-24 23:28:58 +0200

    up

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <header>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="img_logo/vame8.png"></a>
+    <a class="navbar-brand" href="#"><img src="img_logo/vame5.png"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
